f82faf0b7326e0a85061ba70491160a0dbeeccf2
yu <null>
2020-04-12 10:59:55 +0800

    base drawing system

---
+++ attraction/attraction.js
@@ -0,0 +1,40 @@
+var canvas;
+var ctx;
+var things = [];
+
+window.onload = function() {
+	canvas = document.getElementById("canvas");
+	if (canvas.getContext) {
+		ctx = canvas.getContext("2d");
+		window.requestAnimationFrame(draw);
+	}
+	resize();
+	window.addEventListener("resize",resize);
+	canvas.addEventListener("click", function (event) {
+		things.push(new thing(event.x,event.y));
+	});
+}
+
+function resize() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+function draw() {
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.fillStyle = "blue";
+	for (let i = things.length-1; i >= 0; i--) {
+		let thingi = things[i];
+		// draw
+		ctx.beginPath();
+		ctx.arc(thingi.x,thingi.y,32,0,Math.PI*2);
+		ctx.closePath();
+		ctx.fill();
+	}
+	window.requestAnimationFrame(draw);
+}
+
+function thing(x,y) {
+	this.x = x;
+	this.y = y;
+}
