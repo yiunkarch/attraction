6ba0a0328abe8ba69926eab5ca58549b27f7614c
yu <null>
2020-04-12 11:35:05 +0800

    add velocity

--- attraction/attraction.js
+++ attraction/attraction.js
@@ -21,10 +21,18 @@ function resize() {
 }
 
 function draw() {
-	ctx.clearRect(0,0,canvas.width,canvas.height);
+	// clear
+	ctx.fillStyle = "rgb(0,0,0,0.2)";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
 	ctx.fillStyle = "blue";
 	for (let i = things.length-1; i >= 0; i--) {
 		let thingi = things[i];
+		// velocity
+		thingi.x += thingi.vx;
+		thingi.y += thingi.vy;
+		if (thingi.x < 0 || thingi.x > canvas.width || thingi.y < 0 || thingi.y > canvas.height) {
+			things.splice(i,1);
+		}
 		// draw
 		ctx.beginPath();
 		ctx.arc(thingi.x,thingi.y,32,0,Math.PI*2);
@@ -37,4 +45,6 @@ function draw() {
 function thing(x,y) {
 	this.x = x;
 	this.y = y;
+	this.vx = Math.random() * 16 - 8;
+	this.vy = Math.random() * 16 - 8;
 }
