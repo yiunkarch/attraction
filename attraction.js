3a14227644ae40c844ad2e82e05d26150e8e4a40
yu <null>
2020-04-12 13:42:11 +0800

    add randomised mass

--- attraction.js
+++ attraction.js
@@ -32,7 +32,7 @@ function draw() {
 			let thingo = things[o];
 			let dx = thingi.x - thingo.x;
 			let dy = thingi.y - thingo.y;
-			let scale = 64 / (Math.pow(dx,2) + Math.pow(dy,2)); // scale = (root(ma2*mb2)) / (root(d2^2))
+			let scale = Math.pow(thingi.m * thingo.m, 2) / (Math.pow(dx,2) + Math.pow(dy,2)); // scale = (root(ma2*mb2)) / (root(d2^2))
 			dx *= scale;
 			dy *= scale;
 			thingi.vx += -dx;
@@ -40,15 +40,15 @@ function draw() {
 			thingo.vx += dx;
 			thingo.vy += dy;
 		}
-		// apply velocity
-		thingi.x += thingi.vx;
-		thingi.y += thingi.vy;
+		// apply velocity (vx is actually "force on x axis")
+		thingi.x += thingi.vx / thingi.m;
+		thingi.y += thingi.vy / thingi.m;
 		if (thingi.x < 0 || thingi.x > canvas.width || thingi.y < 0 || thingi.y > canvas.height) {
 			things.splice(i,1);
 		}
 		// draw
 		ctx.beginPath();
-		ctx.arc(thingi.x,thingi.y,32,0,Math.PI*2);
+		ctx.arc(thingi.x,thingi.y,thingi.m*16,0,Math.PI*2);
 		ctx.closePath();
 		ctx.fill();
 	}
@@ -60,4 +60,5 @@ function thing(x,y) {
 	this.y = y;
 	this.vx = 0;
 	this.vy = 0;
+	this.m = Math.random()*4;
 }
